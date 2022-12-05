d4d603dc6471f785486939ab3ac7bc4439ce15e1
Elis <emuldaka@gmail.com>
2022-12-05 12:02:34 -0600

    commit tree d8556cc3e7e5868c910fcb85535f5660222af6f6

---
+++ index.js
@@ -0,0 +1,41 @@
+function add() {
+    // we'll fill this in shortly
+  }
+  function subtract() {
+    // we'll fill this in shortly
+  }
+  function multiply() {
+    // we'll fill this in shortly
+  }
+  function divide() {
+    // we'll fill this in shortly
+  
+}function add(a,b) {
+    return a+b;
+     // we'll fill this in shortly
+  }
+  function subtract(a,b) {
+    return a-b;
+    // we'll fill this in shortly
+  }
+  function multiply(a,b) {
+    return a*b;
+    // we'll fill this in shortly
+  }
+  function divide(a,b) {
+    return a/b;
+    // we'll fill this in shortly
+  }
+  function increment(n) {
+    return ++n;
+  }
+function decrement(n) {
+    return --n;
+}
+function makeInt(n) {
+   return parseInt(n,10);
+}
+function preserveDecimal(n) {
+    if (parseFloat(n,10)==NaN){return NaN;}
+    return parseFloat(n,10);
+ }
